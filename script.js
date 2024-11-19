3f7aef002f9473c9b41d4a1f7a7bfbd7c21b5626
Gcraft13 <120202358+Gcraft13@users.noreply.github.com>
2024-11-19 12:47:34 -0500

    add styling for header and sidebar. Make card

--- script.js
+++ script.js
@@ -14,20 +14,9 @@ const Hobbit = new Book("The Hobbit", "J.R.R. Tolkein", 295, "read");
 
 const tacticalBarbell = new Book("Tactical Barbell", "K Black", "150", "read");
 
-Book.prototype.goodBook = function () {
-  console.log("this is a good book!");
-};
-
-myLibrary.push(Hobbit);
-myLibrary.push(tacticalBarbell);
-
-function Player(name, marker) {
-  this.name = name;
-  this.marker = marker;
-  this.sayName = function () {
-    console.log(this.name);
-  };
+function addBooktoLibrary(book) {
+  myLibrary.push(book);
 }
 
+addBooktoLibrary(Hobbit);
 console.log(myLibrary);
-console.log(myLibrary[0].title);
