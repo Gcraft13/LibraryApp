e7a9b9e6e5f93c5d6871dca35f8356ea16e1cb5f
Gcraft13 <120202358+Gcraft13@users.noreply.github.com>
2024-11-20 12:52:41 -0500

    add function for delete buttons on cards

--- script.js
+++ script.js
@@ -1,3 +1,42 @@
+const mainPage = document.querySelector(".main-content");
+const deleteBtns = document.querySelectorAll(".delete");
+const addBtn = document.querySelector(".add");
+const card = document.querySelector(".card-one");
+const deleteBtnSection = document.querySelector(".delete-button");
+
+//delete book
+function deleteBook() {
+  if (card) {
+    card.remove();
+    localStorage.setItem("card", "true");
+  }
+}
+window.addEventListener("load", () => {
+  if (localStorage.getItem("card") == "true") {
+    deleteBook();
+  }
+});
+
+//deleting function applied to all buttons
+
+for (var i = 0; i < deleteBtns.length; i++) {
+  deleteBtns[i].addEventListener("click", function (e) {
+    let parentCard = e.target.parentElement.parentElement;
+    parentCard.remove();
+  });
+}
+
+// for (var i = 0; i < deleteBtns.length; i++) {
+//   deleteBtns[i].addEventListener("click", function (e) {
+//     window.addEventListener("load", () => {
+//       let parentCard = e.target.parentElement.parentElement;
+//       if (localStorage.getItem("card") == "true") {
+//         parentCard.remove();
+//       }
+//     });
+//   });
+// }
+
 const myLibrary = [];
 
 function Book(title, author, pages, read) {
@@ -19,4 +58,3 @@ function addBooktoLibrary(book) {
 }
 
 addBooktoLibrary(Hobbit);
-console.log(myLibrary);
